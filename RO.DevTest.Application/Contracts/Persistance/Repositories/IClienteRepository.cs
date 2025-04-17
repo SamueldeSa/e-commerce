a1ac84010b38f05f2c7451e030e1a3bb5fe7b9c2
Samuel Vitor <samuelsaoliveira@gmail.com>
2025-04-16 22:17:10 -0300

    feat(cliente): implementado clienteService com lÓgica de CRUD

---
+++ RO.DevTest.Application/Contracts/Persistance/Repositories/IClienteRepository.cs
@@ -0,0 +1,17 @@
+﻿using RO.DevTest.Application.DTOs;
+using System.Threading.Tasks;
+using RO.DevTest.Domain.Entities;
+
+namespace RO.DevTest.Application.Contracts.Persistance.Repositories
+{
+    public interface IClienteRepository
+    {
+        Task<Cliente?> GetByIdAsync(Guid id);
+        Task<IEnumerable<Cliente>> GetAllAsync();
+        Task AddAsync(Cliente cliente);
+        void Updade(Cliente cliente);
+        void Remove(Cliente cliente);
+        Task SaveChangesAsync();
+
+    }
+}
---
+++ RO.DevTest.Application/Contracts/Services/ClienteService.cs
@@ -0,0 +1,109 @@
+﻿using RO.DevTest.Application.Contracts.Persistance.Repositories;
+using RO.DevTest.Application.DTOs;
+using RO.DevTest.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RO.DevTest.Application.Contracts.Services
+{
+    internal class ClienteService : IClienteService
+    {
+        private readonly IClienteRepository _clienteRepository;
+
+        public ClienteService(IClienteRepository clienteRepository)
+        {
+
+            _clienteRepository = clienteRepository;
+        }
+
+        public async Task<ClienteDTO> CreateAsync(CreateClienteDto dto)
+        {
+            var Cliente = new Cliente
+            {
+                Id = Guid.NewGuid(),
+                Nome = dto.Nome,
+                Email = dto.Email,
+                Cpf = dto.Cpf,
+                DataNascimento = dto.DataNascimento
+            };
+
+            await _clienteRepository.AddAsync(Cliente);
+            await _clienteRepository.SaveChangesAsync();
+
+            return new ClienteDTO
+            {
+                Id = Cliente.Id,
+                Nome = Cliente.Nome,
+                Email = Cliente.Email,
+                Cpf = Cliente.Cpf,
+                DataNascimento = dto.DataNascimento
+
+            };
+
+        }
+
+        public async Task<bool> DeleteAsync(Guid Id)
+        {
+            var cliente = await _clienteRepository.GetByIdAsync(Id);
+            if (cliente == null) return false;
+
+            _clienteRepository.Remove(cliente);
+            await _clienteRepository.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<IEnumerable<ClienteDTO>> GetAllAsync()
+        {
+            var clientes = await _clienteRepository.GetAllAsync();
+
+            return clientes.Select(c => new ClienteDTO
+            {
+                Id = c.Id,
+                Nome = c.Nome,
+                Email = c.Email,
+                Cpf= c.Cpf,
+                DataNascimento= c.DataNascimento   
+            });
+
+        }
+
+        public async Task<ClienteDTO?> GetByIdAsync(Guid id)
+        {
+            var cliente = await _clienteRepository.GetByIdAsync(id);
+
+            if(cliente == null) return null;
+
+            return new ClienteDTO
+            {
+                Id = cliente.Id,
+                Nome = cliente.Nome,
+                Email = cliente.Email,
+                Cpf = cliente.Cpf,
+                DataNascimento = cliente.DataNascimento
+            };
+
+        }
+
+        public async Task<bool> UpdateAsync(Guid Id, UpdateClienteDto dto)
+        {
+            var cliente = await _clienteRepository.GetByIdAsync(Id);
+            if (cliente == null) return false;
+
+            cliente.Nome = dto.Nome;
+            cliente.Email = dto.Email;
+            cliente.DataNascimento = dto.DataNascimento;
+
+            _clienteRepository.Updade(cliente);
+            await _clienteRepository.SaveChangesAsync();
+
+            return true;
+
+
+        }
+
+
+    }
+}
